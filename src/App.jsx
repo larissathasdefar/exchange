90eb4704b3ade2e735899c655d13afc43c32440c
Larissa Thaís de Farias <larissafarias@vanhack.com>
2019-02-11 00:41:01 -0200

    added routes on project and headbar component to every page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
+import {
+  HashRouter as Router, Route, Switch, Redirect,
+} from 'react-router-dom'
+import Home from './components/Home'
+import Exchange from './components/Exchange'
+import HeadBar from './components/UI/HeadBar'
 
 const App = () => (
-  <div>Hello World</div>
+  <div>
+    <HeadBar />
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/exchange" />} />
+        <Route exact path="/exchange" component={props => <Home {...props} />} />
+        <Route exact path="/exchange/:from/to/:to" component={props => <Exchange {...props} />} />
+      </Switch>
+    </Router>
+  </div>
 )
 
 ReactDom.render(<App />, document.getElementById('root'))
